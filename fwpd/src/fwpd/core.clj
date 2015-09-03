f1d79e0d3010a61c47439d4cc04ac32390c692ab
Matt Gauger <matt.gauger@gmail.com>
2015-09-02 20:56:24 -0500

    Exercises

--- fwpd/src/fwpd/core.clj
+++ fwpd/src/fwpd/core.clj
@@ -46,3 +46,29 @@
 
 ;; (glitter-filter 3 (mapify (parse (slurp filename))))
 
+;;;; Exercises
+;; 1. Turn the result of your glitter filter into a list of names.
+
+(defn suspect-list
+  [filtered-suspects]
+  (map #(% :name) filtered-suspects))
+
+;; (suspect-list (glitter-filter 3 (mapify (parse (slurp filename)))))
+;;=> ("Edward Cullen" "Jacob Black" "Carlisle Cullen")
+
+;; 2. Write a function, append, which will append a new suspect to your list of suspects.
+
+(defn append
+  [lst & ]
+  (apply conj suspects lst))
+
+;; (append (glitter-filter 3 (mapify (parse (slurp filename)))) {:name "Matt" :glitter-index 3})
+;;=> ({:name "Carlisle Cullen", :glitter-index 6} {:name "Jacob Black", :glitter-index 3} {:name "Edward Cullen", :glitter-index 10} {:name "Matt", :glitter-index 3})
+
+;; 3. Write a function, validate, which will check that :name and :glitter-index are present when you append. The validate function should accept two arguments: a map of keywords to validating functions, similar to conversions, and the record to be validated.
+
+(defn validate
+  [validations-map record]
+  ;;(reduce )
+  )
+
