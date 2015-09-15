e7f1081ea43188685fab5d1a9a5b8663c35b1cc6
Matt Gauger <matt.gauger@gmail.com>
2015-09-14 21:06:59 -0500

    Macros example

---
+++ true-ale/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to true-ale
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ true-ale/src/true_ale/core.clj
@@ -0,0 +1,42 @@
+(ns true-ale.core)
+
+(def order-details
+  {:name "Mitchard Blimmons"
+   :email "mitchard.blimmonsexample.com"})
+
+; What we want to see:
+;(validate-order order-details order-details-validations)
+; => {:email ["Your email address doesn't look like an email address."]}
+
+(def order-details-validations
+  {:name ["Please enter a name" not-empty]
+   :email ["Please enter an email address" not-empty
+           "Your email address doesn't look like an email address" #(or (empty? %) (re-seq #"@" %))]})
+
+(defn error-messages-for
+  "Return a seq of error messages"
+  [to-validate message-validator-pairs]
+  (map first (filter #(not ((second %) to-validate))
+                     (partition 2 message-validator-pairs))))
+
+(defn validate
+  "Returns a map with a vector of errors for each key"
+  [to-validate validations]
+  (reduce (fn [errors validation]
+            (let [[fieldname validation-check-groups] validation
+                  value (get to-validate fieldname)
+                  error-messages (error-messages-for value validation-check-groups)]
+              (if (empty? error-messages)
+                errors
+                (assoc errors fieldname error-messages))))
+          {}
+          validations))
+
+(validate order-details order-details-validations)
+
+(defmacro if-valid
+  "Handle validation more concisely"
+  [to-validate validations errors-name & then-else]
+  `(let [~errors-name (validate ~to-validate ~validations)]
+     (if (empty? ~errors-name)
+       ~@then-else)))
